1b1eb6eb6e2e485bf7374e7410933adfbaa7b492
Tyler <Tyler@localhost>
2010-09-30 13:41:47 -0700

    Removed a confusing and ill-thought distanceToDegrees() method.  What I was trying to accomplish is much simpler and only makes sense in a few contexts anyway, so if this is a general function it'll confuse people.

--- src/main/java/simplelatlng/LatLngTool.java
+++ src/main/java/simplelatlng/LatLngTool.java
@@ -48,9 +48,13 @@ public class LatLngTool {
 	}
 
 	/**
-	 * The internal angle for an arc between two points on the surface of a sphere
-	 * in radians. Multiply this value by the sphere's radius to get the length of 
-	 * the arc.
+	 * <p>This "distance" function is mostly for internal use. Most users will simply
+	 * rely upon {@link distance}</p>
+	 * 
+	 * <p>Yields the internal angle for an arc between two points on the surface of a sphere
+	 * in radians. This angle is in the plane of the great circle connecting the two points
+	 * measured from an axis through one of the points and the center of the Earth.
+	 * Multiply this value by the sphere's radius to get the length of the arc.</p>
 	 * 
 	 * @return the internal angle for the arc connecting the two points in radians.
 	 */
@@ -63,31 +67,6 @@ public class LatLngTool {
 		double a = Math.sin(dLatR / 2) * Math.sin(dLatR / 2) + Math.cos(lat1R)
 				* Math.cos(lat2R) * Math.sin(dLngR / 2) * Math.sin(dLngR / 2);
 		return 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	}
-
-	/**
-	 * <p>Find an approximate internal angle which corresponds to an arc of a 
-	 * given distance. Note: values from this function are best used over small 
-	 * distances (maybe within a few hundred kilometers) local to a given point. 
-	 * Since arc per degree latitude is constant over the sphere while arc per 
-	 * degree longitude decreases as you travel towards the poles, this calculation 
-	 * averages the latitude/longitude angle that would correspond to the given arc.</p>
-	 * 
-	 * <p>This function is optionally used to create windows with a desired size.</p>
-	 * 
-	 * @param distance the length of the arc.
-	 * @param unit the unit of measure for the arc length.
-	 * @return the corresponding internal angle in degrees.
-	 */
-	public static double distanceToDegrees(LatLng point, double distance,
-			LengthUnit unit) {
-		/* Approximate by averaging the angle needed for a latitudinal arc and
-		 * a longitudinal arc from the given point.
-		 */
-		double latAngle = distance / LatLngConfig.getEarthRadius(unit);
-		double lngAngle = latAngle
-				/ Math.cos(Math.toRadians(point.getLongitude()));
-		return Math.toDegrees((lngAngle + latAngle) / 2.0);
 	}
 
 	/**
